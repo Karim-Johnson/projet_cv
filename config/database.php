7825b6e76f94a446b90c4571e5d23e954535a514
karimjohnson <kouekamjohnson@gmail.com>
2020-12-05 19:08:03 +0100

    gestion bd

--- config/database.php
+++ config/database.php
@@ -5,8 +5,6 @@
     $username='root';
     $password='';
     $dbName='moncv';
-
-
     try{
         $database=new PDO('mysql:host='.$url.';dbname='.$dbName.';charset=utf8',$username,$password);
         $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
